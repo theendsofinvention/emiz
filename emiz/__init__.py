515b0224a555349274b21392de2f2302c5b229ec
132nd-etcher <132nd-etcher@daribouca.net>
2017-08-19 17:49:04 +0200

    add MissionWeather to exports

--- emiz/__init__.py
+++ emiz/__init__.py
@@ -4,6 +4,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from .miz import Mission, Miz
 from .parking_spots import parkings
+from .mission_weather import MissionWeather
 
 try:
     __version__ = get_distribution('emiz').version
