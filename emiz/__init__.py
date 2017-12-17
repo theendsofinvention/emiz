803d4b2efc7b5a156e2439e7d1986aabb419d6d4
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-17 15:32:23 +0100

    chg: dev: use new packaging system

--- emiz/__init__.py
+++ emiz/__init__.py
@@ -2,18 +2,15 @@
 """
 Etcher's MIZ library
 """
-from pkg_resources import DistributionNotFound, get_distribution
 
 from emiz.edit_miz import edit_miz
 from emiz.weather import build_metar_from_mission
 from emiz.weather.utils import parse_metar_string, retrieve_metar
+from ._version import get_versions
 from .miz import Mission, Miz
 from .parking_spots import parkings
 
-try:
-    __version__ = get_distribution('emiz').version
-except DistributionNotFound:  # pragma: no cover
-    # package is not installed
-    __version__ = 'not installed'
-
 __all__ = ['parse_metar_string', 'retrieve_metar', 'Miz', 'Mission', 'edit_miz', 'build_metar_from_mission']
+
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@
 
 import os
 
-from setuptools import setup
+from pip.req import parse_requirements
+from setuptools import find_packages, setup
+
+import versioneer
+
+requirements = [str(r.req) for r in
+                parse_requirements('requirements.txt', session=False)]
+test_requirements = [str(r.req) for r in
+                     parse_requirements('requirements-dev.txt', session=False)]
 
 
 def read_local_files(*file_paths: str) -> str:
@@ -24,74 +32,42 @@ def read_local_files(*file_paths: str) -> str:
     return '\n' + '\n\n'.join(map(_read_single_file, file_paths))
 
 
-dependency_links = []
-
-# noinspection SpellCheckingInspection
-install_requires = [
-    'metar',
-    'mpmath',
-    'natsort',
-    'requests',
-    'click',
-]
-
-# noinspection SpellCheckingInspection
-test_requires = [
-    'pytest',
-    'coverage',
-    'hypothesis',
-    'pytest-cache',
-    'pytest-catchlog',
-    'pytest-cov',
-    'pytest-pep8',
-    'pytest-pycharm',
-    'datadiff',
-]
-
-dev_requires = []
-
-setup_requires = [
-    'setuptools_scm',
-]
-
-if __name__ == '__main__':
-    setup(
-        name='EMIZ',
-        author='132nd-etcher',
-        zip_safe=False,
-        author_email='emiz@daribouca.net',
-        platforms=['win32'],
-        url=r'https://github.com/132nd-etcher/EMIZ',
-        download_url=r'https://github.com/132nd-etcher/EMIZ/releases',
-        description='Set of tools for the DCS mission builder',
-        license='GPLv3',
-        long_description=read_local_files('README.rst', 'CHANGELOG.rst'),
-        packages=['emiz'],
-        include_package_data=True,
-        install_requires=install_requires,
-        tests_require=test_requires,
-        use_scm_version=True,
-        setup_requires=setup_requires,
-        dependency_links=dependency_links,
-        python_requires='>=3.6',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Topic :: Utilities',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Environment :: Win32 (MS Windows)',
-            'Natural Language :: English',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: Microsoft :: Windows :: Windows 7',
-            'Operating System :: Microsoft :: Windows :: Windows 8',
-            'Operating System :: Microsoft :: Windows :: Windows 8.1',
-            'Operating System :: Microsoft :: Windows :: Windows 10',
-            'Programming Language :: Cython',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: Implementation',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Topic :: Games/Entertainment',
-            'Topic :: Utilities',
-        ],
-    )
+setup(
+    name='EMIZ',
+    author='132nd-etcher',
+    zip_safe=False,
+    author_email='emiz@daribouca.net',
+    platforms=['win32'],
+    url=r'https://github.com/132nd-etcher/EMIZ',
+    download_url=r'https://github.com/132nd-etcher/EMIZ/releases',
+    description='Set of tools for the DCS mission builder',
+    license='GPLv3',
+    long_description=read_local_files('README.rst', 'CHANGELOG.rst'),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    tests_require=test_requirements,
+    python_requires='>=3.6',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Win32 (MS Windows)',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Cython',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Games/Entertainment',
+        'Topic :: Utilities',
+    ],
+)
